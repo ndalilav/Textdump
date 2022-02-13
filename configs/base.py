7fef26b88903f40ee0588b309d2cb08ea6d9b062
ndalilav <ndalila.vinson@gmail.com>
2022-02-14 00:05:01 +0300

    Pushed application codebase

---
+++ configs/base.py
@@ -0,0 +1,14 @@
+import secrets
+import os
+
+class Base:
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+    # SECRET_KEY = os.environ.get("SECRET_KEY")
+    SECRET_KEY = secrets.token_hex(16)
+
+class Development(Base):
+    FLASK_APP = os.environ.get("FLASK_APP")
+    FLASK_ENV = os.environ.get("FLASK_ENV")
+    # SQLALCHEMY_ECHO = True
+    SQLALCHEMY_DATABASE_URI =os.environ.get("SQLALCHEMY_DATABASE_URI")
+    
